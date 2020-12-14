d22c6e484910b5f7ae8a811772b17905b0285c20
rolando <rolandoaguilarmejia@gmail.com>
2020-12-13 20:59:51 -0600

    arreglos

--- controllers/cart.control.php
+++ controllers/cart.control.php
@@ -1,33 +1,32 @@
 <?php 
     require_once 'models/support/cart.model.php';
     function run(){
-        //Inicializacion de Variables
         $viewData = array();
         $viewData["hasItems"]=false;
         $viewData["subtotal"]=0;
         $viewData["newCart"]=array();
         $products = array();
         $viewData["products"] = array();
-
         if(isset($_GET["page"])){
             $viewData["page"]=$_GET["page"];
         }
         if(!isset($_SESSION["cart"])){
             $_SESSION["cart"]=array();
-        //Si existe carrito en la sesión tomar los productos
+        }
+        //echo '<pre>'.print_r($_SESSION["cart"]).'</pre>';
         if(isset($_SESSION["cart"])){
-            //Toma los productos
             $products = getCartItems($viewData["page"]);
-             //Les hace merge a la variable de ViewData
             mergeFullArrayTo($products,$viewData);
             $viewData["subtotal"]=getCartTotal();
             $viewData["subtotal"]= sprintf('%0.2f', $viewData["subtotal"]);
             if($viewData["subtotal"]>0){
                 $viewData["hasItems"]=true;
             }
+        //echo '<pre>'.print_r($viewData["products"]).'</pre>';
         }//Cart Session
         if($_SERVER["REQUEST_METHOD"]=="POST"){
             $varBody = $_POST;
+            //echo '<pre>'.print_r($_POST).'</pre>';
             if(isset($_POST["btnPagar"])){
                 if(empty($_SESSION["cart"]) || $_SESSION["cartSize"]==0){
                     redirectWithMessage("Parece que tu canasta esta vacia", "index.php?page=cartL");
@@ -37,26 +36,22 @@
                 }
                 redirectToUrl("index.php?page=Checkout");
             }
-            //Si el Post es del buton de quitar productos del carrito
             if(isset($_POST["btnAdd"])){
-                
                 plusCart($varBody["cartCod"],$varBody["prdQuantity"]);
                 redirectToUrl("index.php?page=".$viewData["page"]);
             }//btnAdd
-            //Si el Post es del buton de quitar cantidad de productos del carrito
             if(isset($_POST["btnLess"])){
                 removeCart($varBody["cartCod"]);
                 redirectToUrl("index.php?page=".$viewData["page"]);
             }//btnLess
-            //Si el Post es del buton de quitar completamente el producto del carrito
+
             if(isset($_POST["btnTrash"])){
-                //// echo "hola";
                 trashCart($varBody["cartCod"],$varBody["prdQuantity"]);
                 redirectToUrl("index.php?page=".$viewData["page"]);
             }//btnTrash
         }//Server Request
         
-        //// echo '<pre>'.print_r($_SESSION["cart"]).'</pre>';
+        //echo '<pre>'.print_r($_SESSION["cart"]).'</pre>';
         renderizar("cart", $viewData);
     }
     run();
